799a1987924c45d019dc58732bbb9987687b81a5
thierry le gall <thierry28220@live.fr>
2022-11-07 16:26:58 +0100

    ok

--- sujet07112022/Modeles/Categorie.cs
+++ sujet07112022/Modeles/Categorie.cs
@@ -9,7 +9,11 @@ namespace sujet07112022.Modeles
     internal class Categorie
     {
         #region Attributs
-
+        private int _id;
+        private string _libelle;
+        private int _ageMin;
+        private int _ageMax;
+        private List<Licence> _lesLicences;
         public static List<Categorie> CollClasse = new List<Categorie>();
 
         #endregion
@@ -24,11 +28,69 @@ namespace sujet07112022.Modeles
         #endregion
 
         #region Getters/Setters
+        public int GetId()
+        {
+            return _id;
+        }
 
+        public void SetId(int param)
+        {
+            _id = param;
+        }
+
+        public string GetLibelle()
+        {
+            return _libelle;
+        }
+
+        public void SetLibelle(string param)
+        {
+            _libelle = param;
+        }
+        public int GetAgeMin()
+        {
+            return _ageMin;
+        }
+
+        public void SetAgeMin(int param)
+        {
+            _ageMax = param;
+        }
+        public int GetAgeMax()
+        {
+            return _ageMax;
+        }
+
+        public void SetAgeMax(int param)
+        {
+            _ageMax = param;
+        }
+        public List<Licence> GetLesLicences()
+        {
+            return _lesLicences;
+        }
+
+        public void SetLesLicences(List<Licence> param)
+        {
+            _lesLicences = param;
+        }
         #endregion
 
         #region Methodes
+        public List<LigueRegionale> GetListeLigues()
+        {
+            List<LigueRegionale> resultat = new List<LigueRegionale>();
 
+            foreach(Licence uneLicence in this.GetLesLicences())
+            {
+                if (!resultat.Contains(uneLicence.GetLeClub().GetLaLigueRegionale()))
+                {
+                    resultat.Add(uneLicence.GetLeClub().GetLaLigueRegionale());
+                }
+
+            }
+            return resultat;
+        }
         #endregion
     }
 }
